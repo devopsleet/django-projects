065a3888e8e88c900e5b65021a3462706647bc90
gagan <gasingla07@gmail.com>
2026-02-17 14:40:09 -0600

    add blog application

--- blog/models.py
+++ blog/models.py
@@ -3,33 +3,30 @@ from django.db import models
 from django.utils import timezone
 
 
+# Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
-    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateField(auto_now=True)
     status = models.CharField(
         max_length=2,
-        choices= Status,
+        choices=Status,
         default=Status.DRAFT
     )
 
-    def __str__(self):
-        return self.title
-    
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
         ]
-    # Create your models here.
+
+    def __str__(self):
+        return self.title
+    
